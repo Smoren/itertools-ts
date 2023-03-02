4028bf41f17a6ba899f92d9d2c6aff6372f34296
Smoren <ofigate@gmail.com>
2023-03-02 20:22:23 +0300

    Minor changes.

--- src/single.ts
+++ src/single.ts
@@ -1,7 +1,10 @@
 import { toIterable } from "./tools";
 
-export function *map<TInput, TOutput>(data: Iterable<TInput>, mapper: (datum: TInput) => TOutput): Iterable<TOutput> {
-    for (const datum of toIterable(data)) {
-        yield mapper(datum);
-    }
+export function* map<TInput, TOutput>(
+  data: Iterable<TInput>|Iterator<TInput>,
+  mapper: (datum: TInput) => TOutput,
+): Iterable<TOutput> {
+  for (const datum of toIterable(data)) {
+    yield mapper(datum);
+  }
 }
