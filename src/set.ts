4ee00053074c40d094da5865e69e15d4d97820e7
Smoren <ofigate@gmail.com>
2023-03-05 04:39:04 +0300

    Function `set:distinct()` refactored.

--- src/set.ts
+++ src/set.ts
@@ -1,21 +1,17 @@
 import { toIterable } from "./tools";
+import { single } from "./index";
 
 export function *distinct<T>(data: Iterable<T>|Iterator<T>): Iterable<T> {
   const used = new Set();
 
   if (data instanceof Map) {
-    for (const datum of data) {
-      if (!used.has(datum[1])) {
-        yield datum[1];
-        used.add(datum[1]);
-      }
-    }
-  } else {
-    for (const datum of toIterable(data)) {
-      if (!used.has(datum)) {
-        yield datum;
-        used.add(datum);
-      }
+    data = single.map(data, (datum) => datum[1]);
+  }
+
+  for (const datum of toIterable(data)) {
+    if (!used.has(datum)) {
+      yield datum;
+      used.add(datum);
     }
   }
 }
