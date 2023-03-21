f6edb8a1a905a8e5122c5a0762201340a615efaf
Smoren <ofigate@gmail.com>
2023-03-21 12:55:32 +0300

    Transform functions `toSet()` and `toMap()` added and tested; `toIterable()` function extended with supporting records.

--- src/transform.ts
+++ src/transform.ts
@@ -1,15 +1,16 @@
 import { InvalidArgumentError } from "./exceptions";
 import { isIterable, isIterator } from "./summary";
+import { Pair, RecordKey } from './types';
 
 /**
- * Converts collection to Iterable instance.
+ * Converts collection or record to Iterable instance.
  *
  * If instance is already an iterable then just return it.
  *
  * @param collection
  */
 export function toIterable<T>(
-  collection: Iterable<T> | Iterator<T>
+  collection: Iterable<T> | Iterator<T> | Record<RecordKey, unknown>
 ): Iterable<T> {
   if (isIterable(collection)) {
     return collection as Iterable<T>;
@@ -21,6 +22,16 @@ export function toIterable<T>(
         return collection as Iterator<T>;
       },
     };
+  }
+
+  if (typeof collection === 'object' && collection !== null) {
+    return (function* () {
+      for (const key in collection) {
+        if (collection.hasOwnProperty(key)) {
+          yield [key, (collection as Record<string|number|symbol, unknown>)[key]];
+        }
+      }
+    })() as Iterable<T>;
   }
 
   throw new InvalidArgumentError(
@@ -64,6 +75,36 @@ export function toArray<T>(collection: Iterable<T> | Iterator<T>): Array<T> {
   const result = [];
   for (const item of toIterable(collection)) {
     result.push(item);
+  }
+  return result;
+}
+
+/**
+ * Converts collection of key-value pairs to Map.
+ *
+ * @param pairs
+ */
+export function toMap<TKey, TValue>(
+  pairs: Iterable<[TKey, TValue]> | Iterator<[TKey, TValue]> | Record<RecordKey, unknown>
+): Map<TKey, TValue> {
+  const result: Map<TKey, TValue> = new Map();
+  for (const [key, value] of toIterable(pairs)) {
+    result.set(key, value);
+  }
+  return result;
+}
+
+/**
+ * Converts given collection to Set.
+ *
+ * @param collection
+ */
+export function toSet<T>(
+  collection: Iterable<T> | Iterator<T>
+): Set<T> {
+  const result: Set<T> = new Set();
+  for (const datum of toIterable(collection)) {
+    result.add(datum);
   }
   return result;
 }
