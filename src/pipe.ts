1d5b77cc96c99f5de6a09b69b61f1a2b69c2684b
Smoren <ofigate@gmail.com>
2024-12-20 10:31:43 +0300

    More pipe functions notations

--- src/pipe.ts
+++ src/pipe.ts
@@ -17,9 +17,14 @@ export function createPipe<T1, T2, T3, T4, T5>(...operations: [PipeOperation<T1,
 export function createPipe<T1, T2, T3, T4, T5, T6>(...operations: [PipeOperation<T1, T2>, PipeOperation<T2, T3>, PipeOperation<T3, T4>, PipeOperation<T4, T5>, PipeOperation<T5, T6>]): PipeOperation<T1, T6>;
 export function createPipe<T1, T2, T3, T4, T5, T6, T7>(...operations: [PipeOperation<T1, T2>, PipeOperation<T2, T3>, PipeOperation<T3, T4>, PipeOperation<T4, T5>, PipeOperation<T5, T6>, PipeOperation<T6, T7>]): PipeOperation<T1, T7>;
 export function createPipe<T1, T2, T3, T4, T5, T6, T7, T8>(...operations: [PipeOperation<T1, T2>, PipeOperation<T2, T3>, PipeOperation<T3, T4>, PipeOperation<T4, T5>, PipeOperation<T5, T6>, PipeOperation<T6, T7>, PipeOperation<T7, T8>]): PipeOperation<T1, T8>;
-export function createPipe<T1, T2, T3, T4, T5, T6, T7, T8, T9>(...operations: [PipeOperation<T1, T2>, PipeOperation<T2, T3>, PipeOperation<T3, T4>, PipeOperation<T4, T5>, PipeOperation<T5, T6>, PipeOperation<T6, T7>, PipeOperation<T7, T8>, PipeOperation<T9, T9>]): PipeOperation<T1, T9>;
+export function createPipe<T1, T2, T3, T4, T5, T6, T7, T8, T9>(...operations: [PipeOperation<T1, T2>, PipeOperation<T2, T3>, PipeOperation<T3, T4>, PipeOperation<T4, T5>, PipeOperation<T5, T6>, PipeOperation<T6, T7>, PipeOperation<T7, T8>, PipeOperation<T8, T9>]): PipeOperation<T1, T9>;
 export function createPipe<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(...operations: [PipeOperation<T1, T2>, PipeOperation<T2, T3>, PipeOperation<T3, T4>, PipeOperation<T4, T5>, PipeOperation<T5, T6>, PipeOperation<T6, T7>, PipeOperation<T7, T8>, PipeOperation<T8, T9>, PipeOperation<T9, T10>]): PipeOperation<T1, T10>;
-
+export function createPipe<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(...operations: [PipeOperation<T1, T2>, PipeOperation<T2, T3>, PipeOperation<T3, T4>, PipeOperation<T4, T5>, PipeOperation<T5, T6>, PipeOperation<T6, T7>, PipeOperation<T7, T8>, PipeOperation<T8, T9>, PipeOperation<T9, T10>, PipeOperation<T10, T11>]): PipeOperation<T1, T11>;
+export function createPipe<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>(...operations: [PipeOperation<T1, T2>, PipeOperation<T2, T3>, PipeOperation<T3, T4>, PipeOperation<T4, T5>, PipeOperation<T5, T6>, PipeOperation<T6, T7>, PipeOperation<T7, T8>, PipeOperation<T8, T9>, PipeOperation<T9, T10>, PipeOperation<T10, T11>, PipeOperation<T11, T12>]): PipeOperation<T1, T12>;
+export function createPipe<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>(...operations: [PipeOperation<T1, T2>, PipeOperation<T2, T3>, PipeOperation<T3, T4>, PipeOperation<T4, T5>, PipeOperation<T5, T6>, PipeOperation<T6, T7>, PipeOperation<T7, T8>, PipeOperation<T8, T9>, PipeOperation<T9, T10>, PipeOperation<T10, T11>, PipeOperation<T11, T12>, PipeOperation<T12, T13>]): PipeOperation<T1, T13>;
+export function createPipe<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>(...operations: [PipeOperation<T1, T2>, PipeOperation<T2, T3>, PipeOperation<T3, T4>, PipeOperation<T4, T5>, PipeOperation<T5, T6>, PipeOperation<T6, T7>, PipeOperation<T7, T8>, PipeOperation<T8, T9>, PipeOperation<T9, T10>, PipeOperation<T10, T11>, PipeOperation<T11, T12>, PipeOperation<T12, T13>, PipeOperation<T13, T14>]): PipeOperation<T1, T14>;
+export function createPipe<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>(...operations: [PipeOperation<T1, T2>, PipeOperation<T2, T3>, PipeOperation<T3, T4>, PipeOperation<T4, T5>, PipeOperation<T5, T6>, PipeOperation<T6, T7>, PipeOperation<T7, T8>, PipeOperation<T8, T9>, PipeOperation<T9, T10>, PipeOperation<T10, T11>, PipeOperation<T11, T12>, PipeOperation<T12, T13>, PipeOperation<T13, T14>, PipeOperation<T14, T15>]): PipeOperation<T1, T15>;
+export function createPipe<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>(...operations: [PipeOperation<T1, T2>, PipeOperation<T2, T3>, PipeOperation<T3, T4>, PipeOperation<T4, T5>, PipeOperation<T5, T6>, PipeOperation<T6, T7>, PipeOperation<T7, T8>, PipeOperation<T8, T9>, PipeOperation<T9, T10>, PipeOperation<T10, T11>, PipeOperation<T11, T12>, PipeOperation<T12, T13>, PipeOperation<T13, T14>, PipeOperation<T14, T15>, PipeOperation<T15, T16>]): PipeOperation<T1, T16>;
 /**
  * Creates a synchronous pipe that processes an input through a sequence of operations.
  *
@@ -38,15 +43,6 @@ export function createPipe(...operations: PipeOperation<any, any>[]): any {
   };
 }
 
-// export function createPipe<TFlow extends any[]>(...operations: PipeOperationSequence<TFlow>): Pipe<TFlow> {
-//   return (input: TFlow[0]) => {
-//     return operations.reduce(
-//       (prevResult, operation) => operation(prevResult),
-//       input,
-//     ) as Last<TFlow>;
-//   };
-// }
-
 export function createAsyncPipe<TFlow extends any[]>(...operations: AsyncPipeOperationSequence<TFlow>): AsyncPipe<TFlow>;
 export function createAsyncPipe<T1>(...operations: []): AsyncPipeOperation<T1, T1>;
 export function createAsyncPipe<T1, T2>(...operations: [AsyncPipeOperation<T1, T2>]): AsyncPipeOperation<T1, T2>;
@@ -56,8 +52,14 @@ export function createAsyncPipe<T1, T2, T3, T4, T5>(...operations: [AsyncPipeOpe
 export function createAsyncPipe<T1, T2, T3, T4, T5, T6>(...operations: [AsyncPipeOperation<T1, T2>, AsyncPipeOperation<T2, T3>, AsyncPipeOperation<T3, T4>, AsyncPipeOperation<T4, T5>, AsyncPipeOperation<T5, T6>]): AsyncPipeOperation<T1, T6>;
 export function createAsyncPipe<T1, T2, T3, T4, T5, T6, T7>(...operations: [AsyncPipeOperation<T1, T2>, AsyncPipeOperation<T2, T3>, AsyncPipeOperation<T3, T4>, AsyncPipeOperation<T4, T5>, AsyncPipeOperation<T5, T6>, AsyncPipeOperation<T6, T7>]): AsyncPipeOperation<T1, T7>;
 export function createAsyncPipe<T1, T2, T3, T4, T5, T6, T7, T8>(...operations: [AsyncPipeOperation<T1, T2>, AsyncPipeOperation<T2, T3>, AsyncPipeOperation<T3, T4>, AsyncPipeOperation<T4, T5>, AsyncPipeOperation<T5, T6>, AsyncPipeOperation<T6, T7>, AsyncPipeOperation<T7, T8>]): AsyncPipeOperation<T1, T8>;
-export function createAsyncPipe<T1, T2, T3, T4, T5, T6, T7, T8, T9>(...operations: [AsyncPipeOperation<T1, T2>, AsyncPipeOperation<T2, T3>, AsyncPipeOperation<T3, T4>, AsyncPipeOperation<T4, T5>, AsyncPipeOperation<T5, T6>, AsyncPipeOperation<T6, T7>, AsyncPipeOperation<T7, T8>, AsyncPipeOperation<T9, T9>]): AsyncPipeOperation<T1, T9>;
+export function createAsyncPipe<T1, T2, T3, T4, T5, T6, T7, T8, T9>(...operations: [AsyncPipeOperation<T1, T2>, AsyncPipeOperation<T2, T3>, AsyncPipeOperation<T3, T4>, AsyncPipeOperation<T4, T5>, AsyncPipeOperation<T5, T6>, AsyncPipeOperation<T6, T7>, AsyncPipeOperation<T7, T8>, AsyncPipeOperation<T8, T9>]): AsyncPipeOperation<T1, T9>;
 export function createAsyncPipe<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(...operations: [AsyncPipeOperation<T1, T2>, AsyncPipeOperation<T2, T3>, AsyncPipeOperation<T3, T4>, AsyncPipeOperation<T4, T5>, AsyncPipeOperation<T5, T6>, AsyncPipeOperation<T6, T7>, AsyncPipeOperation<T7, T8>, AsyncPipeOperation<T8, T9>, AsyncPipeOperation<T9, T10>]): AsyncPipeOperation<T1, T10>;
+export function createAsyncPipe<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(...operations: [AsyncPipeOperation<T1, T2>, AsyncPipeOperation<T2, T3>, AsyncPipeOperation<T3, T4>, AsyncPipeOperation<T4, T5>, AsyncPipeOperation<T5, T6>, AsyncPipeOperation<T6, T7>, AsyncPipeOperation<T7, T8>, AsyncPipeOperation<T8, T9>, AsyncPipeOperation<T9, T10>, AsyncPipeOperation<T10, T11>]): AsyncPipeOperation<T1, T11>;
+export function createAsyncPipe<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>(...operations: [AsyncPipeOperation<T1, T2>, AsyncPipeOperation<T2, T3>, AsyncPipeOperation<T3, T4>, AsyncPipeOperation<T4, T5>, AsyncPipeOperation<T5, T6>, AsyncPipeOperation<T6, T7>, AsyncPipeOperation<T7, T8>, AsyncPipeOperation<T8, T9>, AsyncPipeOperation<T9, T10>, AsyncPipeOperation<T10, T11>, AsyncPipeOperation<T11, T12>]): AsyncPipeOperation<T1, T12>;
+export function createAsyncPipe<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>(...operations: [AsyncPipeOperation<T1, T2>, AsyncPipeOperation<T2, T3>, AsyncPipeOperation<T3, T4>, AsyncPipeOperation<T4, T5>, AsyncPipeOperation<T5, T6>, AsyncPipeOperation<T6, T7>, AsyncPipeOperation<T7, T8>, AsyncPipeOperation<T8, T9>, AsyncPipeOperation<T9, T10>, AsyncPipeOperation<T10, T11>, AsyncPipeOperation<T11, T12>, AsyncPipeOperation<T12, T13>]): AsyncPipeOperation<T1, T13>;
+export function createAsyncPipe<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>(...operations: [AsyncPipeOperation<T1, T2>, AsyncPipeOperation<T2, T3>, AsyncPipeOperation<T3, T4>, AsyncPipeOperation<T4, T5>, AsyncPipeOperation<T5, T6>, AsyncPipeOperation<T6, T7>, AsyncPipeOperation<T7, T8>, AsyncPipeOperation<T8, T9>, AsyncPipeOperation<T9, T10>, AsyncPipeOperation<T10, T11>, AsyncPipeOperation<T11, T12>, AsyncPipeOperation<T12, T13>, AsyncPipeOperation<T13, T14>]): AsyncPipeOperation<T1, T14>;
+export function createAsyncPipe<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>(...operations: [AsyncPipeOperation<T1, T2>, AsyncPipeOperation<T2, T3>, AsyncPipeOperation<T3, T4>, AsyncPipeOperation<T4, T5>, AsyncPipeOperation<T5, T6>, AsyncPipeOperation<T6, T7>, AsyncPipeOperation<T7, T8>, AsyncPipeOperation<T8, T9>, AsyncPipeOperation<T9, T10>, AsyncPipeOperation<T10, T11>, AsyncPipeOperation<T11, T12>, AsyncPipeOperation<T12, T13>, AsyncPipeOperation<T13, T14>, AsyncPipeOperation<T14, T15>]): AsyncPipeOperation<T1, T15>;
+export function createAsyncPipe<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>(...operations: [AsyncPipeOperation<T1, T2>, AsyncPipeOperation<T2, T3>, AsyncPipeOperation<T3, T4>, AsyncPipeOperation<T4, T5>, AsyncPipeOperation<T5, T6>, AsyncPipeOperation<T6, T7>, AsyncPipeOperation<T7, T8>, AsyncPipeOperation<T8, T9>, AsyncPipeOperation<T9, T10>, AsyncPipeOperation<T10, T11>, AsyncPipeOperation<T11, T12>, AsyncPipeOperation<T12, T13>, AsyncPipeOperation<T13, T14>, AsyncPipeOperation<T14, T15>, AsyncPipeOperation<T15, T16>]): AsyncPipeOperation<T1, T16>;
 
 /**
  * Creates an asynchronous pipe that processes an input through a sequence of operations.
@@ -77,15 +79,3 @@ export function createAsyncPipe(...operations: AsyncPipeOperation<any, any>[]): 
     ) as Last<AsyncPipeOperation<any, any>[]>;
   };
 }
-
-// export function createAsyncPipe<TFlow extends any[]>(
-//   ...operations: AsyncPipeOperationSequence<TFlow>
-// ): AsyncPipe<TFlow> {
-//   return async (input: TFlow[0]) => {
-//     return await reduce.toValueAsync(
-//       operations,
-//       async (prevResult, operation) => await operation(prevResult),
-//       input,
-//     ) as Last<TFlow>;
-//   };
-// }
